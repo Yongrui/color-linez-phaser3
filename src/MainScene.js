e4cf41a01d9a781e2d98e96bea9d93835dfd7fa8
Yongrui <liangyongrui@gmail.com>
2018-10-30 18:41:32 +0800

    fix dev mode

---
+++ src/MainScene.js
@@ -0,0 +1,12 @@
+export default class MainScene extends Phaser.Scene
+{
+	preload ()
+	{
+		this.load.atlas('emoji', '../assets/atlases/emoji.png', '../assets/atlases/emoji.json');
+	}
+
+	create ()
+	{
+		
+	}
+}
